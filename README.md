1d6b9353429d1113e41901e6cd62483f092ce960
Yalton <73573710+Yalton@users.noreply.github.com>
2022-09-14 17:28:17 -0700

    nmap now optional

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # System Security Auditing script
 
-## Script to audit the security of a Linux system 
+## About
+- Script to audit the security of a Linux system 
 - Originally intented to be ran as a cronjob but can also be ran manually
 - Further auditting software will be added to this script in the future.
+### Usage 
+- ./systemScan.sh <OUTPUT_DIR> <1:nmapscan || 0:no_nmpascan> <IP_RANGE>
+- EX: ./systemScan.sh /opt/SecurityAudits 1 192.168.50.0/24
--- systemScan.sh
+++ systemScan.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 SCANDIR=$1
-IPRANGE=$2
 LOGFILE=$(date +"%d-%m-%Y").log
+if [[ "$2" -eq 1 ]]; then 
+    IPRANGE=$3
+fi
 
 #Run Chkrootkit
 if [ ! -d "$SCANDIR/chkrootkit" ]; then
@@ -25,9 +27,10 @@ touch $SCANDIR/lynisaudits/$LOGFILE
 lynis --quick 2>&1 > $SCANDIR/lynisaudits/$LOGFILE
 
 #Run nmap & Send results to NAS
-#Run Lynis & Send results to NAS
-if [ ! -d "$SCANDIR/nmapscans" ]; then
-    mkdir $SCANDIR/nmapscans
+if [[ "$2" -eq 1 ]]; then 
+    if [ ! -d "$SCANDIR/nmapscans" ]; then
+        mkdir $SCANDIR/nmapscans
+    fi
+    touch $SCANDIR/nmapscans/$LOGFILE
+    nmap -p 1-65535 -T4 -A -v $IPRANGE > $SCANDIR/nmapscans/$LOGFILE
 fi
-touch $SCANDIR/nmapscans/$LOGFILE
-nmap -p 1-65535 -T4 -A -v $IPRANGE > $SCANDIR/nmapscans/$LOGFILE
